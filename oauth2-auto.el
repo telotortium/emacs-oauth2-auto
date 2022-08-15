39a26b0a37db34ad1d3a517bd7aca23a9df55107
Robert Irelan <rirelan@gmail.com>
2022-08-15 13:46:07 -0700

    Add advice to throw error on plstore keys being written in wrong place
    For background see
    https://github.com/rhaps0dy/emacs-oauth2-auto/issues/6.

--- oauth2-auto.el
+++ oauth2-auto.el
@@ -169,6 +169,22 @@ from PLIST if non-nil.  The return value is intended to be stored in plstore."
             (oauth2-auto--plstore-write username provider plist)))
       (plstore-close plstore))))
 
+(defun oauth2-auto--insert-break-on-secret-entries (&rest args)
+  "Break if trying to insert secret entries outside of plstore buffer.
+ARGS are those passed to ‘insert’.
+
+This function is added as before advice to ‘insert’ to attempt to reproduce and
+fix https://github.com/rhaps0dy/emacs-oauth2-auto/issues/6."
+  (when
+      (and
+       (stringp (buffer-file-name))
+       (not (file-equal-p oauth2-auto-plstore (buffer-file-name)))
+       (equal ";;; secret entries\n" (nth 0 args))
+       (backtrace-frames 'oauth2-auto--plstore-write))
+    (error "BUG: Attempted to write ‘oauth2-auto’ keys to %s, not ‘oauth2-auto-plstore’ (%s).  Please report to https://github.com/rhaps0dy/emacs-oauth2-auto/issues/6."
+           (buffer-file-name) oauth2-auto-plstore)))
+(advice-add #'insert :before #'oauth2-auto--insert-break-on-secret-entries)
+
 (defun oauth2-auto--plstore-read (username provider)
   "Read the data for USERNAME and PROVIDER from the cache, else from plstore.
 Cache data if a miss occurs."
