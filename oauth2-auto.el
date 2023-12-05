96d45242c6570f1e8cd0b4cd8f7dea5fb9392fb1
dawg <code@dawg.eu>
2023-12-05 22:55:46 +0100

    fix copyright statement (#19)
    * fix copyright statement
    
    - seems unlikely that this has been [assigned to the FSF](https://www.gnu.org/licenses/why-assign)
    - introduced with [first commit](https://github.com/telotortium/emacs-oauth2-auto/commit/178ca24b0c61c5a9fb286d13e98626978992eda5#diff-3c11c8a0c1f1ace4d5eb5d1bd981417eaaa91d99768dcfc964be2a497bca5a4aR3)
    
    * update license banner
    
    * Update oauth2-auto.el
    
    Co-authored-by: Adrià Garriga-Alonso <adria.garriga@gmail.com>
    
    ---------
    
    Co-authored-by: Robert Irelan <rirelan@gmail.com>
    Co-authored-by: Adrià Garriga-Alonso <adria.garriga@gmail.com>

--- oauth2-auto.el
+++ oauth2-auto.el
@@ -1,6 +1,6 @@
 ;;; oauth2-auto.el --- Automatically refreshing OAuth 2.0 tokens -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2011-2021 Free Software Foundation, Inc
+;; Copyright (C) 2022-2023 Adrià Garriga-Alonso, Robert Irelan
 
 ;; Author: Adrià Garriga-Alonso <adria.garriga@gmail.com>
 ;; URL: https://github.com/rhaps0dy/emacs-oauth2-auto
@@ -8,20 +8,18 @@
 ;; Keywords: comm oauth2
 ;; Package-Requires: ((emacs "26.1") (aio "1.0") (alert "1.2") (dash "2.19"))
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
